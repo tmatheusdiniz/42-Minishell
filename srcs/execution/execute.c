ab28b9f8b81322fbe26a89d9274e4a690aa22625
user00100010 <mreinald@student.42porto.com>
2025-06-19 23:40:03 +0100

    fixing conflict

--- srcs/execution/execute.c
+++ srcs/execution/execute.c
@@ -6,11 +6,8 @@
 /*   By: cda-fons <cda-fons@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/15 20:23:13 by mreinald          #+#    #+#             */
-<<<<<<< HEAD
 /*   Updated: 2025/06/16 00:36:24 by mreinald         ###   ########.fr       */
-=======
 /*   Updated: 2025/06/19 12:11:29 by cda-fons         ###   ########.fr       */
->>>>>>> e2cef89ee2eba442aaea493f207022d4f23647b8
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +15,7 @@
 
 void	ft_execution(t_shell *shell, t_exec *exec_node)
 {
-<<<<<<< HEAD
 	(void)exec_node;
 	if (check_whatis(shell->root) == EXEC)
 		exit(0);
-=======
-	(void)shell;
-	(void)exec_node;
->>>>>>> e2cef89ee2eba442aaea493f207022d4f23647b8
 }
